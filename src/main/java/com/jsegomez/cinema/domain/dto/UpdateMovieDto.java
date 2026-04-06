2a039e5759dcb7335faf03f803bedf4d1685580b
Josué Eliezer Gómez Soto <jsegomez06@gmail.com>
2026-04-05 21:41:17 -0600

    Modified: RestControllerAdvice

--- src/main/java/com/jsegomez/cinema/domain/dto/UpdateMovieDto.java
+++ src/main/java/com/jsegomez/cinema/domain/dto/UpdateMovieDto.java
@@ -1,11 +1,16 @@
 package com.jsegomez.cinema.domain.dto;
 
 import com.jsegomez.cinema.persistence.entity.enums.MovieRating;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public record UpdateMovieDto(
+
+        @Size(min = 1, message = "Title must not be empty if provided")
         String title,
+
         LocalDate releaseDate,
+
         MovieRating rating
 ) { }
